d1e65512c36680504741b657bd0fdf80e52973b8
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-01-14 14:51:00 +0100

    chore: created checkout-short form

---
+++ checkout-short.php
@@ -0,0 +1,130 @@
+[MM_Custom_Decision tstmls='true']
+<style>.testimonial {display: block;}</style>
+[/MM_Custom_Decision]
+[MM_Custom_Decision tstmls='false']
+<style>.testimonial {display: none;}</style>
+[/MM_Custom_Decision]
+
+<div class="mm-checkoutContainer">[MM_Form type='checkout']
+[MM_Form_Message type='error']
+<div class="mm_left_column">
+
+[MM_Form_Section type='accountInfo']
+<div id="mm-account-information-section" class="mm-checkoutInfoBlock">
+<h3>Account Information</h3>
+<p class="mm-formField"><label>Email:</label>
+[MM_Form_Field type='input' name='email' customAttributes='placeholder="Email Address"']</p>
+<p id="mm-formfield-width-left" class="mm-formField"><label>First Name:</label>
+[MM_Form_Field type='input' name='firstName' customAttributes='placeholder="First Name"']</p>
+<p id="mm-formfield-width-right" class="mm-formField"><label>Last Name:</label>
+[MM_Form_Field type='input' name='lastName' customAttributes='placeholder="Last Name"']</p>
+<p id="mm-formfield-width-left" class="mm-formField"><label>Password:</label>
+[MM_Form_Field type='input' name='password' customAttributes='placeholder="Password"']</p>
+[MM_Form_Field type='hidden' name='phone' isRequired='false' customAttributes='placeholder="Phone Number"']</p>
+
+</div>
+[/MM_Form_Section]
+[MM_Form_Section type='billingInfo']
+<div id="mm-billing-information-section" class="mm-checkoutInfoBlock">
+<h3>Billing Details</h3>
+<p class="mm-ccLogos"><img src="https://redpillquants.com/wp-content/plugins/membermouse/resources/images/cclogos.gif" alt="Visa, Master Card, American Express, Discover" width="199" height="30" /></p>
+
+<div id="mm-creditcard-field">
+<p class="mm-formField"><label>Credit Card:</label>
+[MM_Form_Field name='ccNumber' customAttributes='placeholder="Credit Card Number"']</p>
+
+</div>
+<div id="mm-security-field">
+<p class="mm-formField"><label>Security Code:</label>
+[MM_Form_Field name='ccSecurityCode' customAttributes='placeholder="Security Code"']</p>
+
+</div>
+<div id="mm-exp-field">
+<p id="mm-checkout-expiration-date" class="mm-checkout-expiration-date mm-formField"><label>Expiration Date: </label>
+[MM_Form_Field name='ccExpirationDate']</p>
+
+</div>
+[MM_Form_Field type='hidden' name='billingAddress' isRequired='false' customAttributes='placeholder="Billing Address"']</p>
+[MM_Form_Field type='hidden' name='billingCity' isRequired='false' customAttributes='placeholder="Billing City"']</p>
+[MM_Form_Field type='hidden' name='billingState' isRequired='false' customAttributes='placeholder="Billing State"']</p>
+[MM_Form_Field type='hidden' name='billingZipCode' isRequired='false' customAttributes='placeholder="Billing Zip Code"']</p>
+[MM_Form_Field type='hidden' name='billingCountry' isRequired='false']</p>
+
+</div>
+[/MM_Form_Section]
+[MM_Form_Section type='shippingInfo']
+<div id="mm-shipping-information-section" class="mm-checkoutInfoBlock">
+<p id="mm-shipping-method-block" class="mm-formField"><label>Shipping Method:</label>
+[MM_Form_Field name='shippingMethod']</p>
+[MM_Form_Field name='shippingSameAsBilling']</p>
+[MM_Form_Subsection type='shippingAddress']
+<div>
+[MM_Form_Field name='shippingAddress' customAttributes='placeholder="Shipping Address"']</p>
+[MM_Form_Field name='shippingCity' customAttributes='placeholder="Shipping City"']</p>
+[MM_Form_Field name='shippingState' customAttributes='placeholder="Shipping State"']</p>
+[MM_Form_Field name='shippingZipCode' customAttributes='placeholder="Shipping Zip Code"']</p>
+[MM_Form_Field name='shippingCountry']</p>
+
+</div>
+[/MM_Form_Subsection]
+
+</div>
+[/MM_Form_Section]
+[MM_Form_Section type='coupon']
+<div id="mm-coupon-block" class="mm-couponSection mm-checkoutInfoBlock">
+<h3>Coupons</h3>
+<p class="mm-formField">[MM_Form_Field name='couponCode']
+<a class="mm-button" href="[MM_Form_Button type='applyCoupon']">Apply Coupon</a></p>
+[MM_Form_Message type='couponSuccess']
+[MM_Form_Message type='couponError']
+
+</div>
+[/MM_Form_Section]
+
+</div>
+<div class="mm_right_column">
+<div class="mm-checkoutSection2">
+<h2>[MM_Form_Data name='productName' useAccessName='false']</h2>
+<p class="mm-productDesc">[MM_Form_Data name='productDescription']</p>
+
+<ul>
+ 	<li style="list-style-type: none;">
+<ul>
+ 	<li><span class="mm-prices">Product Price:</span> [MM_Form_Data name='productPriceDescription']</li>
+</ul>
+</li>
+</ul>
+[MM_Order_Decision isShippable='true']
+<ul>
+ 	<li style="list-style-type: none;">
+<ul>
+ 	<li><span class="mm-prices">Shipping Price:</span> [MM_Form_Data name='shippingPrice']</li>
+</ul>
+</li>
+</ul>
+[/MM_Order_Decision]
+[MM_Order_Decision isDiscounted='true']
+<ul>
+ 	<li style="list-style-type: none;">
+<ul>
+ 	<li><span class="mm-prices">Discount:</span> [MM_Form_Data name='discount']</li>
+</ul>
+</li>
+</ul>
+[/MM_Order_Decision]
+<ul>
+ 	<li><span class="mm-prices">Total Price:</span> [MM_Form_Data name='totalPrice']</li>
+</ul>
+</div>
+<div class="mm-purchaseSection">
+<div class="mm-paymentbuttons">[MM_Form_Button type='all' label='Submit Order' color='orange']</div>
+</div>
+<div class="testimonial">
+<h3>What others are saying</h3>
+[tvo_shortcode id=1512]
+
+</div>
+</div>
+[/MM_Form]
+
+</div>
