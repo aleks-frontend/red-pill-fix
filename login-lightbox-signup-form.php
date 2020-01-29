7a0ae96e4d4c09078a2fff84ec81dc1849720051
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-01-29 14:05:13 +0100

    initial: created login-lightbox-signup-form

---
+++ login-lightbox-signup-form.php
@@ -0,0 +1,35 @@
+<div id="optin-newsletter-form-lightbox" style="white-space: nowrap;">
+<script src="https://www.google.com/recaptcha/api.js"></script>
+<form id="lead-gen-form-lightbox" action="https://sendy.redpillquants.com/subscribe" method="POST" accept-charset="utf-8">
+	<input style="width: 175px;
+		background-color : #e1e1e1; 
+		border-radius: 25px;
+    overflow: hidden;
+    font-family: FaktPro-Blond;
+    font-size: 17px;
+    padding-top: 13px !important;
+    margin-top: -10px !important;
+    padding-left: 29px !important;
+    padding-bottom: 13px !important;
+    border:0px;" type="text" name="name" id="name" placeholder="Your Name">
+	<input style="width: 175px;
+		background-color : #e1e1e1; 						
+		border-radius: 25px;
+    overflow: hidden;
+    font-family: FaktPro-Blond;
+    font-size: 17px;
+    padding-top: 13px !important;
+    margin-top: -10px !important;
+		margin-left: 10px !important;						
+    padding-left: 29px !important;
+    padding-bottom: 13px !important;
+    border: 0px;" type="email" name="email" id="email" placeholder="Your Email">
+	<div style="display:none;">
+	<label for="hp">HP</label><br>
+	<input type="text" name="hp" id="hp">
+	</div>
+	<input type="hidden" name="subform" value="yes">
+	<input type="hidden" id="list" name="list" value="7QHnJzbdbxYPtJjqkMZU5w">
+	<button id="captcha4" style="width: 205px; border: medium none; border-radius: 25px; overflow: hidden; font-size: 29px; font-family: FaktPro-Medium; font-weight: 400; padding-top: 5px; padding-bottom: 5px; margin-top: 0px; margin-bottom: 10px; margin-left: 10px; color: rgb(255, 255, 255); background-color: rgb(224, 56, 74);" onmouseover="this.style.backgroundColor='#000000'" onmouseout="this.style.backgroundColor='#e0384a'" class="g-recaptcha" data-sitekey="6Lf3y8IUAAAAAEE0pMTHZjRT4DKc-RPolb1Gyjbn" data-callback="onSubmitLeadGenLightbox">Get It Now!</button>
+</form>
+</div>
