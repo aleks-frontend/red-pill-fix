ef67c51298e79c199b9c31a4a37600d822b5c94c
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-01-30 08:44:11 +0100

    chore: created lightbox-form-1 page

---
+++ lightbox-form-1.php
@@ -0,0 +1,32 @@
+<div id="optin-newsletter-form-lightbox">
+<script src="https://www.google.com/recaptcha/api.js"></script>
+<form id="lead-gen-form-lightbox" action="https://sendy.redpillquants.com/subscribe" method="POST" accept-charset="utf-8">
+	<input style="width: 300px;
+		border-radius: 25px;
+    overflow: hidden;
+    font-family: FaktPro-Blond;
+    font-size: 17px;
+    padding-top: 12px !important;
+    margin-top: 13px !important;
+    padding-left: 29px !important;
+    padding-bottom: 12px !important;
+    border:0px;" type="text" name="name" id="name" placeholder="Your Name"><br>
+	<input style="width: 300px;
+		border-radius: 25px;
+    overflow: hidden;
+    font-family: FaktPro-Blond;
+    font-size: 17px;
+    padding-top: 12px !important;
+    margin-top: 13px !important;
+    padding-left: 29px !important;
+    padding-bottom: 12px !important;
+    border: 0px;" type="email" name="email" id="email" placeholder="Your Email"><br>
+	<div style="display:none;">
+	<label for="hp">HP</label><br>
+	<input type="text" name="hp" id="hp">
+	</div>
+	<input type="hidden" name="subform" value="yes">
+	<input type="hidden" id="list" name="list" value="7QHnJzbdbxYPtJjqkMZU5w">
+	<button id="captcha4" style="width: 330px; border: medium none; border-radius: 25px; overflow: hidden; font-size: 29px; font-family: FaktPro-Medium; font-weight: 400; padding-top: 5px; padding-bottom: 5px; margin-top: 18px; margin-bottom: 10px; color: rgb(255, 255, 255); background-color: rgb(224, 56, 74);" onmouseover="this.style.backgroundColor='#000000'" onmouseout="this.style.backgroundColor='#e0384a'" class="g-recaptcha" data-sitekey="6Lf3y8IUAAAAAEE0pMTHZjRT4DKc-RPolb1Gyjbn" data-callback="onSubmitLeadGenLightbox">Get It Now!</button>
+</form>
+</div>
