14a67de142b725b89cbc55a65894bf2e17e60d61
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-01-30 09:46:48 +0100

    chore: fixed indentation

--- footer-form.php
+++ footer-form.php
@@ -1,1 +1,23 @@
-<div id="optin-newsletter-form-thrive-footer"><script src="https://www.google.com/recaptcha/api.js"></script><form id="lead-gen-form-footer" action="https://sendy.redpillquants.com/subscribe" method="POST" accept-charset="utf-8"><input style="width: 300px;border-radius: 25px; overflow: hidden; font-family: FaktPro-Blond; font-size: 17px; padding-top: 15px !important; margin-top: 13px !important; padding-left: 29px !important; padding-bottom: 15px !important; border: 0px" type="text" name="name" placeholder="Your Name"><br><input style="width: 300px;border-radius: 25px; overflow: hidden; font-family: FaktPro-Blond; font-size: 17px; padding-top: 15px !important; margin-top: 13px !important; padding-left: 29px !important; padding-bottom: 15px !important; border:0px" type="email" name="email" placeholder="Your Email"><br><div style="display:none;"><label for="hp">HP</label><br><input type="text" name="hp"></div><input type="hidden" name="subform" value="yes"><input type="hidden" name="list" value="7QHnJzbdbxYPtJjqkMZU5w"><button id="captcha3" style="width: 330px; border: none; border-radius: 25px; overflow: hidden; font-size: 19px; font-family: FaktPro-Medium; font-weight: 400; padding-top: 11px; padding-bottom: 11px; margin-top: 13px; margin-bottom: 10px; color: rgb(255, 255, 255); background-color: rgb(224, 56, 74);" onmouseover="this.style.backgroundColor='#000000'" onmouseout="this.style.backgroundColor='#e0384a'" class="g-recaptcha" data-sitekey="6Lf3y8IUAAAAAEE0pMTHZjRT4DKc-RPolb1Gyjbn" data-callback="onSubmitLeadGenFooter">Sign Up Now!</button></form></div>
+<div id="optin-newsletter-form-thrive-footer">
+    <script src="https://www.google.com/recaptcha/api.js"></script>
+    <form id="lead-gen-form-footer" action="https://sendy.redpillquants.com/subscribe" method="POST"
+        accept-charset="utf-8">
+        <input
+            style="width: 300px;border-radius: 25px; overflow: hidden; font-family: FaktPro-Blond; font-size: 17px; padding-top: 15px !important; margin-top: 13px !important; padding-left: 29px !important; padding-bottom: 15px !important; border: 0px"
+            type="text" name="name" placeholder="Your Name"><br><input
+            style="width: 300px;border-radius: 25px; overflow: hidden; font-family: FaktPro-Blond; font-size: 17px; padding-top: 15px !important; margin-top: 13px !important; padding-left: 29px !important; padding-bottom: 15px !important; border:0px"
+            type="email" name="email" placeholder="Your Email"><br>
+        <div style="display:none;">
+            <label for="hp">HP</label><br>
+            <input type="text" name="hp">
+        </div>
+        <input type="hidden" name="subform" value="yes">
+        <input type="hidden" name="list" value="7QHnJzbdbxYPtJjqkMZU5w">
+        <button
+            id="captcha3"
+            style="width: 330px; border: none; border-radius: 25px; overflow: hidden; font-size: 19px; font-family: FaktPro-Medium; font-weight: 400; padding-top: 11px; padding-bottom: 11px; margin-top: 13px; margin-bottom: 10px; color: rgb(255, 255, 255); background-color: rgb(224, 56, 74);"
+            onmouseover="this.style.backgroundColor='#000000'" onmouseout="this.style.backgroundColor='#e0384a'"
+            class="g-recaptcha" data-sitekey="6Lf3y8IUAAAAAEE0pMTHZjRT4DKc-RPolb1Gyjbn"
+            data-callback="onSubmitLeadGenFooter">Sign Up Now!</button>
+    </form>
+</div>
