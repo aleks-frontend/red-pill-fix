8000437c92e200827e9a5f67c9b2e65a47ee6f26
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-02-10 14:07:18 +0100

    chore: created homepage-signup-form

---
+++ homepage-signup-form.php
@@ -0,0 +1,40 @@
+<div id="optin-newsletter-form-main" style="display: inline-block; white-space: nowrap; position: relative"><script src="https://www.google.com/recaptcha/api.js"></script>
+<form id="lead-gen-form-main" action="https://sendy.redpillquants.com/subscribe" method="POST" accept-charset="utf-8">
+<div class="csc">
+<div class="colm thc">
+	<input style="width: 150px;
+		border-radius: 25px;
+    overflow: hidden;
+    font-family: FaktPro-Blond;
+    font-size: 17px;
+    padding-top: 12px !important;
+    padding-left: 29px !important;
+    padding-bottom: 12px !important;
+    margin-bottom: 8px;
+    border: 0px" type="text" name="name" id="name" placeholder="Your Name">
+</div>
+<div class="colm thc">	
+	<input style="width: 150px;
+		border-radius: 25px;
+    overflow: hidden;
+    font-family: FaktPro-Blond;
+    font-size: 17px;
+    padding-top: 12px !important;
+    margin-left: 11px;
+    margin-bottom: 8px;
+    padding-left: 29px !important;
+    padding-bottom: 12px !important;
+    border: 0px" type="email" name="email" id="email" placeholder="Your Email">
+	<div style="display:none;">
+	<label for="hp">HP</label><br>
+	<input type="text" name="hp" id="hp">
+	</div>
+	<input type="hidden" name="subform" value="yes">
+	<input type="hidden" id="list" name="list" value="7QHnJzbdbxYPtJjqkMZU5w">
+</div>
+<div class="colm thc">		
+	<button id="captcha1" style="width: 180px; border: none; border-radius: 25px; overflow: hidden; font-size: 18px; font-family: FaktPro-Normal; font-weight: 400; padding-top: 10px; margin-left: 24px; padding-bottom: 11px; color: rgb(255, 255, 255); background-color: rgb(224, 56, 74);" onmouseover="this.style.backgroundColor='#000000'" onmouseout="this.style.backgroundColor='#e0384a'" class="g-recaptcha" data-sitekey="6Lf3y8IUAAAAAEE0pMTHZjRT4DKc-RPolb1Gyjbn" data-callback="onSubmitLeadGenMain">Subscribe Now</button>
+</div>
+</div>
+</form>
+</div>
