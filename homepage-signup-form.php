b573c30f8d143b1209b1204e2a30ebbd341ad6ce
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2020-02-10 14:10:22 +0100

    chore: homepage signup form scrollbar fix

--- homepage-signup-form.php
+++ homepage-signup-form.php
@@ -11,6 +11,7 @@
     padding-left: 29px !important;
     padding-bottom: 12px !important;
     margin-bottom: 8px;
+    max-width: 100%;
     border: 0px" type="text" name="name" id="name" placeholder="Your Name">
 </div>
 <div class="colm thc">	
@@ -22,6 +23,7 @@
     padding-top: 12px !important;
     margin-left: 11px;
     margin-bottom: 8px;
+    max-width: 100%;
     padding-left: 29px !important;
     padding-bottom: 12px !important;
     border: 0px" type="email" name="email" id="email" placeholder="Your Email">
@@ -33,7 +35,7 @@
 	<input type="hidden" id="list" name="list" value="7QHnJzbdbxYPtJjqkMZU5w">
 </div>
 <div class="colm thc">		
-	<button id="captcha1" style="width: 180px; border: none; border-radius: 25px; overflow: hidden; font-size: 18px; font-family: FaktPro-Normal; font-weight: 400; padding-top: 10px; margin-left: 24px; padding-bottom: 11px; color: rgb(255, 255, 255); background-color: rgb(224, 56, 74);" onmouseover="this.style.backgroundColor='#000000'" onmouseout="this.style.backgroundColor='#e0384a'" class="g-recaptcha" data-sitekey="6Lf3y8IUAAAAAEE0pMTHZjRT4DKc-RPolb1Gyjbn" data-callback="onSubmitLeadGenMain">Subscribe Now</button>
+	<button id="captcha1" style="width: 180px; max-width: 100%; border: none; border-radius: 25px; overflow: hidden; font-size: 18px; font-family: FaktPro-Normal; font-weight: 400; padding-top: 10px; margin-left: 24px; padding-bottom: 11px; color: rgb(255, 255, 255); background-color: rgb(224, 56, 74);" onmouseover="this.style.backgroundColor='#000000'" onmouseout="this.style.backgroundColor='#e0384a'" class="g-recaptcha" data-sitekey="6Lf3y8IUAAAAAEE0pMTHZjRT4DKc-RPolb1Gyjbn" data-callback="onSubmitLeadGenMain">Subscribe Now</button>
 </div>
 </div>
 </form>
